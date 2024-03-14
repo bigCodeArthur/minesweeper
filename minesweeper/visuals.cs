ec3bd0fa5a31dd987cade8594807f1f71a865acb
guapito <arthur_bosch@hotmail.com>
2024-03-14 21:22:53 +0100

    added the visuals class

---
+++ minesweeper/visuals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minesweeper
+{
+    internal class visuals
+    {
+        public static void drawField()
+        {
+
+        }
+    }
+}
